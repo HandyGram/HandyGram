2cab0b1288b0f7157c10c9ebd7ce2f379909fbb1
tdrkDev <teledurak@gmail.com>
2023-05-21 17:49:33 +0300

    widgets/list: add ScalingListBuilder

--- lib/src/widgets/list.dart
+++ lib/src/widgets/list.dart
@@ -66,3 +66,73 @@ class _ScalingListState extends State<ScalingList> {
     );
   }
 }
+
+class ScalingListBuilder extends StatefulWidget {
+  const ScalingListBuilder({
+    Key? key,
+    required this.itemBuilder,
+    required this.itemCount,
+  }) : super(key: key);
+
+  final Widget? Function(BuildContext, int) itemBuilder;
+  final int itemCount;
+
+  @override
+  State<StatefulWidget> createState() => _ScalingListBuilderState();
+}
+
+class _ScalingListBuilderState extends State<ScalingListBuilder> {
+  final scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (session.isSquareScreen) {
+      return Padding(
+        padding: const EdgeInsets.all(10),
+        child: StretchingOverscrollIndicator(
+          axisDirection: scrollController.positions.isEmpty
+              ? AxisDirection.down
+              : scrollController.position.axisDirection,
+          child: ListView.builder(
+            controller: scrollController,
+            padding: EdgeInsets.all(session.isSquareScreen ? 0 : 20),
+            itemCount: widget.itemCount,
+            itemBuilder: widget.itemBuilder,
+          ),
+        ),
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: RotaryScrollWrapper(
+        rotaryScrollbar: RotaryScrollbar(
+          controller: scrollController,
+          width: 4,
+          autoHideDuration: const Duration(seconds: 1),
+        ),
+        child: StretchingOverscrollIndicator(
+          axisDirection: scrollController.positions.isEmpty
+              ? AxisDirection.down
+              : scrollController.position.axisDirection,
+          child: ListView.builder(
+            controller: scrollController,
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+              bottom: MediaQuery.of(context).size.width / 2,
+              top: MediaQuery.of(context).size.width / 4,
+            ),
+            itemCount: widget.itemCount,
+            itemBuilder: widget.itemBuilder,
+          ),
+        ),
+      ),
+    );
+  }
+}
