4ed7a9a25c0c1a251b0381f2b03db5017db1116c
tdrkDev <teledurak@gmail.com>
2023-05-21 17:43:44 +0300

    misc/tdlib_utils: add permissions checker function

--- lib/src/misc/tdlib_utils.dart
+++ lib/src/misc/tdlib_utils.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:handygram/src/tdlib/td_api.dart' as tdlib;
+import 'package:handygram/src/telegram/session.dart';
 
 extension MessageSenderWithId on tdlib.MessageSender {
   int getSenderId() => switch (this) {
@@ -12,4 +14,55 @@ extension InputMessageWithText on tdlib.InputMessageContent {
         tdlib.InputMessageText(text: var text) => text.text,
         _ => "unsupported",
       };
+}
+
+bool? canSendMessages(int chatId, [WidgetRef? ref]) {
+  var ci = ref?.watch(session.chatsInfoCacheP) ?? session.chatsInfoCache;
+  var chat = ci.maybeGet(chatId);
+  if (chat == null) {
+    ci.get(chatId);
+    return null;
+  }
+
+  tdlib.ChatPermissions perms = chat.permissions;
+  var type = chat.type;
+  if (type is tdlib.ChatTypeSupergroup) {
+    var si = ref?.watch(session.supergroupsP) ?? session.supergroups;
+    var s = si.maybeGet(type.supergroupId);
+    if (s == null) {
+      si.get(type.supergroupId);
+      return null;
+    }
+
+    if (type.isChannel) {
+      var status = s.status;
+      if (status is tdlib.ChatMemberStatusAdministrator) {
+        return status.rights.canPostMessages;
+      } else if (status is! tdlib.ChatMemberStatusCreator) {
+        return false;
+      }
+      return true;
+    } else {
+      var status = s.status;
+      if (status is tdlib.ChatMemberStatusRestricted) {
+        return status.permissions.canSendMessages;
+      }
+    }
+  } else if (type is tdlib.ChatTypeBasicGroup) {
+    var bi = ref?.watch(session.basicGroupsP) ?? session.basicGroups;
+    var b = bi.maybeGet(type.basicGroupId);
+    if (b == null) {
+      bi.get(type.basicGroupId);
+      return null;
+    }
+
+    var status = b.status;
+    if (status is tdlib.ChatMemberStatusRestricted) {
+      return status.permissions.canSendMessages;
+    }
+  } else if (type is tdlib.ChatTypePrivate) {
+    return true;
+  }
+
+  return perms.canSendMessages;
 }
