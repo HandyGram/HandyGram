24c178338064d45ddb23e1e7c2a08108ed3dd545
Roman Rihter <teledurak@gmail.com>
2024-08-07 22:35:47 +0300

    TdlibUpdatesProviderTypicalWrappers: do not depend on certain provider type

--- lib/src/common/tdlib/misc/updaters_wrappers.dart
+++ lib/src/common/tdlib/misc/updaters_wrappers.dart
@@ -3,11 +3,12 @@ import 'dart:async';
 import 'package:handy_tdlib/api.dart' as td;
 import 'package:handygram/src/common/exceptions/tdlib_core_exception.dart';
 import 'package:handygram/src/common/log/log.dart';
-import 'package:handygram/src/common/tdlib/providers/templates/updates_provider.dart';
+import 'package:handygram/src/common/tdlib/client/structures/base_provider.dart';
+import 'package:handygram/src/common/tdlib/providers/templates/attachable_box.dart';
 import 'package:meta/meta.dart';
 
 // ignore: missing_override_of_must_be_overridden
-mixin TdlibUpdatesProviderTypicalWrappers<T> on TdlibDataUpdatesProvider<T> {
+mixin TdlibUpdatesProviderTypicalWrappers on TdlibDataProvider, AttachableBox {
   static const String tag = "TdlibGetWrapper";
 
   @protected
@@ -23,7 +24,7 @@ mixin TdlibUpdatesProviderTypicalWrappers<T> on TdlibDataUpdatesProvider<T> {
   }) async {
     if (transform == null) assert(S == R, "S and R are not same types");
 
-    final obj = await box.invoke(function);
+    final obj = await box!.invoke(function);
     if (obj is! S) {
       if (obj is td.TdError) {
         l.e(tag, "Failed to get $S [${obj.code}]: ${obj.message}");
@@ -37,9 +38,10 @@ mixin TdlibUpdatesProviderTypicalWrappers<T> on TdlibDataUpdatesProvider<T> {
   }
 
   @protected
-  Future<void> tdlibOkActionWrapper<A>(td.TdFunction function) async {
-    final obj = await box.invoke(function);
-    if (obj is! td.Ok) {
+  Future<void> tdlibOkActionWrapper<A extends td.TdObject>(
+      td.TdFunction function) async {
+    final obj = await box?.invoke(function);
+    if (obj is! A && obj is! td.Ok) {
       if (obj is td.TdError) {
         l.e(tag, "Failed to perform $function [${obj.code}]: ${obj.message}");
         throw TdlibCoreException.fromTd(tag, obj);
