90077472ed67067a09a3d28f5dcf2664a2363f3d
Roman Rihter <teledurak@gmail.com>
2024-09-30 18:29:45 +0300

    misc/strings: small refactoring

--- lib/src/common/misc/strings.dart
+++ lib/src/common/misc/strings.dart
@@ -16,10 +16,23 @@ extension StringDuration on int {
     final seconds = this % 60;
     final minutes = this ~/ 60 % 60;
     final hours = this ~/ 3600;
-    return (hours > 0 ? "$hours:" : "") +
-        (minutes > 0 ? "$minutes:" : "") +
+    final minutesPadded = "$minutes".padLeft(2, '0');
+    final hoursPadded = "$hours".padLeft(2, '0');
+    final secondsPadded = "$seconds".padLeft(2, '0');
+    return (hours > 0 ? "$hoursPadded:" : "") +
+        (minutes > 0 ? "$minutesPadded:" : "") +
         (minutes == 0 && hours == 0
             ? "$seconds ${AppLocalizations.current.secondsCountPlural(seconds)}"
-            : "$seconds");
+            : secondsPadded);
+  }
+
+  String get shortDurationFromSeconds {
+    final seconds = this % 60;
+    final minutes = this ~/ 60 % 60;
+    final hours = this ~/ 3600;
+    final minutesPadded = "$minutes".padLeft(2, '0');
+    final hoursPadded = "$hours".padLeft(2, '0');
+    final secondsPadded = "$seconds".padLeft(2, '0');
+    return "${this ~/ 3600 > 0 ? "$hoursPadded:" : ""}$minutesPadded:$secondsPadded";
   }
 }
